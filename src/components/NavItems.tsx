3bc2eab92fced682d79a829bf51128f1eef6e5a5
single-byte <joharkhan1999@gmail.com>
2024-02-06 13:47:48 +0500

    Add dropdown close functionality

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,33 @@
 "use client"
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const[activeIndex, setActiveIndex] = useState<null | number>(null);
   const isAnyOpen = activeIndex !== null;
 
+  // close the dropdown when clicking outside
+  const navRef = useRef<HTMLDivElement | null>();
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  // close the dropdown when pressing the escape key
+  useEffect(() => {
+    const handler = (e:KeyboardEvent)=>{
+      if (e.key === 'Escape') {
+        setActiveIndex(null)
+      }
+    }
+    document.addEventListener("keydown", handler);
+
+    return ()=>{
+      document.removeEventListener("keydown", handler);
+    }
+  }, []);
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {
         PRODUCT_CATEGORIES.map((category, index) => {
           const handleOpen = () => {
