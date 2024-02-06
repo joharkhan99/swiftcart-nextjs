98d2f54f6bb2e47add7a0e074656e57b13e9631e
single-byte <joharkhan1999@gmail.com>
2024-02-06 13:19:34 +0500

    Refactor NavItems component to handle opening and closing of categories

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,31 @@
 "use client"
+import { PRODUCT_CATEGORIES } from "@/config";
 import { useState } from "react";
+import NavItem from "./NavItem";
 
 const NavItems = () => {
   const[activeIndex, setActiveIndex] = useState<null | number>(null);
-  
+  const isAnyOpen = activeIndex !== null;
+
   return (
-    <div className="flex gap-4 h-full"></div>
+    <div className="flex gap-4 h-full">
+      {
+        PRODUCT_CATEGORIES.map((category, index) => {
+          const handleOpen = () => {
+            if (activeIndex === index) {
+              setActiveIndex(null)
+            } else {
+              setActiveIndex(index)
+            }
+          }
+          const isOpen = activeIndex === index;
+
+          return (
+            <NavItem category={category} handleOpen={handleOpen} isOpen={isOpen} key={index} isAnyOpen={isAnyOpen} />
+          )
+        })
+      }
+    </div>
   )
 }
 
